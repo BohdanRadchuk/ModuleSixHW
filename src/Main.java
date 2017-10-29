5a0c8ac5aa518aeaa47b6c0c883329726f252778
BohdanRadchuk <shpionuse@gmail.com>
2017-10-29 17:58:50 +0200

    linked start

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        MyArrayList myList = new MyArrayList();
+       /* MyArrayList myList = new MyArrayList();
         myList.add(1);
         myList.add(2);
         myList.add(3);
@@ -15,7 +15,10 @@ public class Main {
         myList.clear();
         myList.show();
         System.out.println();
-        System.out.println("list.size = " + myList.size());
+        System.out.println("list.size = " + myList.size());*/
+
+        MyLinkedList myLinkedList = new MyLinkedList(10);
+        myLinkedList.add(15);
 
     }
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,5 +1,29 @@
-import java.util.LinkedList;
+
 
 public class MyLinkedList<T> {
-    LinkedList T = new LinkedList();
+    public T value= (String) 0;
+    T first;
+    T last;
+    public int length;
+    public MyLinkedList next;
+    public MyLinkedList current;
+    public MyLinkedList prev;
+
+    public MyLinkedList(T value) {
+
+        current.value = value;
+
+       /* current.next = null;
+        current.prev = null;*/
+        this.length = 1;
+
+    }
+    public void add(T value){
+
+        current.prev = current;
+        current.value = value;
+        current.next = null;
+        length++;
+
+    }
 }
